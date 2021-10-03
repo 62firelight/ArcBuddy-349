430e861330988b555fd84024437f0da746ddbb7d
Luke Tang <tanlu824@student.otago.ac.nz>
2021-10-04 11:51:19 +1300

    Added error data field with relevant messages

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   title = 'Arc Buddy';
 
   name!: string;
+  error!: string;
 
   constructor(private nameService: NameService) { }
 
@@ -17,7 +18,7 @@ export class AppComponent {
     var nameId = this.name.split("#", 2);
 
     if (nameId.length != 2) {
-      console.log("Error occurred when parsing Bungie Name");
+      this.error = `Error occurred when parsing Bungie Name. A Bungie Name should formatted similarly to "name#1234".`;
       return;
     }
 
@@ -28,6 +29,8 @@ export class AppComponent {
 
     this.nameService.getName(name, id).subscribe((result) => {
       console.log(result);
+    }, (error) => {
+      this.error = `Couldn't find requested Bungie Name. Are you sure that ${this.name} is a registered Bungie.net user?`;
     });
   }
 }
