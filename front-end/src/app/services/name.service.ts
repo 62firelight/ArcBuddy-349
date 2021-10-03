cdd04c8b4d18aa184c166fb70873ac85b569f8db
Luke Tang <tanlu824@student.otago.ac.nz>
2021-10-04 11:44:40 +1300

    Added getName() method to Name service

--- front-end/src/app/services/name.service.ts
+++ front-end/src/app/services/name.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NameService {
+  private apiUrl = 'http://localhost:3000';
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  getName(name: string, id: string): Observable<string> {
+    const url = `${this.apiUrl}/api/players/${name}/${id}`;
+    return this.http.get<string>(url);
+  }
 }
