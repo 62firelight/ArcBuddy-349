7e4264343064a564ba01eae9d4bb1c3774034763
Luke Tang <tanlu824@student.otago.ac.nz>
2021-10-04 11:29:20 +1300

    Added functionality to search bar for testing

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Arc Buddy';
+
+  name!: string;
+
+  onSubmit(): void {
+    console.log("Searching for " + this.name);
+  }
 }
