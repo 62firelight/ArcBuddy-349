29ea88ca8bda9016a469d8153ce8b9c4e19ef2fd
Luke Tang <tanlu824@student.otago.ac.nz>
2021-10-05 11:39:48 +1300

    Added route for stats component

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StatsComponent } from './components/stats/stats.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'stats', component: StatsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
