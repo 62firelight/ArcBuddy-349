32719790f9a0b1efaf1e1a223cfc284e685fefc7
Luke Tang <tanlu824@student.otago.ac.nz>
2021-10-05 11:49:47 +1300

    Added untested getStats() method

--- front-end/src/app/services/profile.service.ts
+++ front-end/src/app/services/profile.service.ts
@@ -15,4 +15,9 @@ export class ProfileService {
     const url = `${this.apiUrl}/api/players/${name}/${id}`;
     return this.http.get<Profile>(url);
   }
+
+  getStats(type: string, id: string): Observable<Object> {
+    const url = `${this.apiUrl}/api/players/account/${type}/${id}`;
+    return this.http.get<Object>(url);
+  }
 }
