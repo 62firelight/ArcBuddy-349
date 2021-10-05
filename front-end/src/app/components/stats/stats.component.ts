a0de3c2824114aac04e8844e7856d580bbb7dbd7
Luke Tang <tanlu824@student.otago.ac.nz>
2021-10-05 13:29:21 +1300

    Gave the stats a way to retrieve dynamic data

--- front-end/src/app/components/stats/stats.component.ts
+++ front-end/src/app/components/stats/stats.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Profile } from 'src/app/Profile';
+import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-stats',
@@ -10,9 +11,14 @@ export class StatsComponent implements OnInit {
 
   @Input() profile!: Profile;
 
-  constructor() { }
+  constructor(private profileService: ProfileService) { }
 
   ngOnInit(): void {
+    this.profileService.getStats(this.profile.membershipType, this.profile.membershipId)
+    .subscribe((result) => {
+      this.profile.characterStats = result;
+      console.log(this.profile.characterStats);
+    })
   }
 
 }
