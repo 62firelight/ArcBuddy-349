11e1fe9db7d0645dd890c3073769509471079d6e
Luke Tang <tanlu824@student.otago.ac.nz>
2021-10-05 14:13:14 +1300

    Restored dynamic functionality to stats page

--- front-end/src/app/components/stats/stats.component.ts
+++ front-end/src/app/components/stats/stats.component.ts
@@ -14,35 +14,36 @@ export class StatsComponent implements OnInit {
   constructor(private profileService: ProfileService) { }
 
   ngOnInit(): void {
-    // this.profileService.getStats(this.profile.membershipType, this.profile.membershipId)
-    // .subscribe((result) => {
-    //   this.profile.characterStats = result;
-    //   console.log(this.profile.characterStats);
-    // })
+    this.profileService.getStats(this.profile.membershipType, this.profile.membershipId)
+    .subscribe((result) => {
+      this.profile.characterStats = result;
+      console.log(this.profile.characterStats);
+    })
 
-    this.profile = {
-      displayName: "62firelight#8173",
-      iconPath: "",
-      membershipId: "222",
-      membershipType: "3",
-      characterStats: {}
-    };
+    // mock data for faster testing
+    // this.profile = {
+    //   displayName: "62firelight#8173",
+    //   iconPath: "",
+    //   membershipId: "222",
+    //   membershipType: "3",
+    //   characterStats: {}
+    // };
 
-    this.profile.characterStats = {
-      "Activities Cleared": '1711', 
-      "Activities Entered": '3761', 
-      "Assists": '49808',
-      "stuff1": "333",
-      "stuff2": "333",
-      "stuff3": "333",
-      "stuff4": "333",
-      "stuff5": "333",
-      "stuff6": "333",
-      "stuff7": "333",
-      "stuff8": "333",
-      "stuff9": "333",
-      "stuff10": "333",
-    }
+    // this.profile.characterStats = {
+    //   "Activities Cleared": '1711', 
+    //   "Activities Entered": '3761', 
+    //   "Assists": '49808',
+    //   "stuff1": "333",
+    //   "stuff2": "333",
+    //   "stuff3": "333",
+    //   "stuff4": "333",
+    //   "stuff5": "333",
+    //   "stuff6": "333",
+    //   "stuff7": "333",
+    //   "stuff8": "333",
+    //   "stuff9": "333",
+    //   "stuff10": "333",
+    // }
   }
 
 }
