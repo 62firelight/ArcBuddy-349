2339bc003653e886db5e1101c09228ef9bf04c67
Luke <lktfirelight@gmail.com>
2022-07-27 21:38:59 +1200

    Implemented neater server initialization process

--- back-end/server.js
+++ back-end/server.js
@@ -16,10 +16,16 @@ app.use(cors());
 const apiUrl = `http://localhost:${port}`;
 const mongoUri = 'mongodb://127.0.0.1:27017';
 
+let destiny = undefined;
+exports.getDestiny = function getDestiny() {
+    return destiny;
+}
+
 let noDb = false;
 exports.getNoDb = function getNoDb() {
     return noDb;
 }
+
 // read command line arguments if they were passed in
 if (process.argv.length > 2) {
     const myArgs = process.argv.slice(2);
@@ -34,62 +40,42 @@ if (process.argv.length > 2) {
     }
 }
 
-let destiny = undefined;
-exports.getDestiny = function getDestiny() {
-    return destiny;
-}
-const createClient = async () => {
-    try {
-        const apiKey = process.env.ARC_KEY;
-
-        return new destinyApi({
-            key: apiKey
-        });
-    } catch (error) {
-        console.log(error);
-    }
-};
-
+// define routes
 app.get('/', (req, res) => {
     res.send(`
       Welcome to the API homepage for Arc Buddy! There's nothing special to see here right now.
       `)
 });
-
 require('./routes/destiny.routes.js')(app);
 require('./routes/profiles.routes.js')(app);
 
-// initialize server
-
-// connect to MongoDB database
-// if connection fails, then fall back to in-memory database
-console.log(`Attempting to connect to MongoDB server at ${mongoUri}...`);
-mongoose.connect(mongoUri, { serverSelectionTimeoutMS: 2000 })
-    .then(() => {
+const initializeServer = async () => {
+    // connect to MongoDB database
+    // if connection fails, then fall back to in-memory database
+    try {
+        console.log(`Attempting to connect to MongoDB server at ${mongoUri}...`);
+        await mongoose.connect(mongoUri, { serverSelectionTimeoutMS: 2000 });
         console.log('Successfully connected to MongoDB through Mongoose.\n');
-    })
-    .catch((error) => {
-        // console.log(error);
+    } catch (error) {
         console.log(`Couldn't connect to MongoDB through Mongoose. Using in-memory database.\n`);
         noDb = true;
-        // process.exit();
-    })
-    .finally(() => {
-        // construct API wrapper object for calling the Bungie.net API
-        // if connection fails, then terminate the server immediately
+    }
+
+    // construct API wrapper object for calling the Bungie.net API
+    // if connection fails, then terminate the server immediately
+    try {
         console.log('Attempting to connect to Bungie.net API...')
-        createClient()
-            .then((destinyClient) => {
-                destiny = destinyClient;
-                console.log('Successfully connected to Bungie.net API.\n');
+        destiny = await new destinyApi({ key: process.env.ARC_KEY });
+        console.log('Successfully connected to Bungie.net API.\n');
+    } catch (error) {
+        console.error('Failed to connect to Bungie.net API. Terminating server.\n');
+        process.exit();
+    }
+    // 3, '4611686018468181342', '2305843009301648414' (Exo Hunter)
 
-                // 3, '4611686018468181342', '2305843009301648414' (Exo Hunter)
-
-                app.listen(port, () => {
-                    console.log(`Server running at ${apiUrl}.`);
-                });
-            })
-            .catch((error) => {
-                console.log('Failed to connect to Bungie.net API.\n');
-            })
+    app.listen(port, () => {
+        console.log(`Server running at ${apiUrl}.`);
     });
+};
+
+initializeServer();
