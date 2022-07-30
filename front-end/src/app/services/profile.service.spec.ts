7e8c3157344b628afcc63bd7dc75172bd63a7a6b
Luke Tang <tanlu824@student.otago.ac.nz>
2022-07-30 17:58:00 +1200

    Injected HttpClient and HttpTestingController

--- front-end/src/app/services/profile.service.spec.ts
+++ front-end/src/app/services/profile.service.spec.ts
@@ -1,16 +1,28 @@
 import { TestBed } from '@angular/core/testing';
 
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
 import { ProfileService } from './profile.service';
 
 describe('NameService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
   let service: ProfileService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
     service = TestBed.inject(ProfileService);
+
+    // Inject the HTTP service and test controller for each test
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController =  TestBed.inject(HttpTestingController);
   });
 
-  // it('should be created', () => {
-  //   expect(service).toBeTruthy();
-  // });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
 });
