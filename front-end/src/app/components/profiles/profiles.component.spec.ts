59b2b0503d15004cadf2b6d5e12069fa1f678c41
Luke Tang <tanlu824@student.otago.ac.nz>
2022-07-30 19:04:05 +1200

    Injected fake ProfileService in ProfilesComponent

--- front-end/src/app/components/profiles/profiles.component.spec.ts
+++ front-end/src/app/components/profiles/profiles.component.spec.ts
@@ -1,14 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { Profile } from 'src/app/Profile';
+import { ProfileService } from 'src/app/services/profile.service';
+import testData from 'src/app/services/test-data';
 
 import { ProfilesComponent } from './profiles.component';
 
 describe('ProfilesComponent', () => {
+  const testProfiles: Profile[] = [testData, testData, testData];
+
   let component: ProfilesComponent;
   let fixture: ComponentFixture<ProfilesComponent>;
 
+  let fakeProfileService: ProfileService;
+
   beforeEach(async () => {
+    // Create fake profile service
+    fakeProfileService = jasmine.createSpyObj<ProfileService>(
+      'ProfileService',
+      {
+        getProfiles: of(testProfiles),
+        getProfile: undefined,
+        addProfile: undefined,
+        updateProfile: undefined,
+        deleteProfile: undefined
+      }
+    );
+
     await TestBed.configureTestingModule({
-      declarations: [ ProfilesComponent ]
+      declarations: [ ProfilesComponent ],
+      providers: [
+        // Use fake instead of original
+        { provide: ProfileService, useValue: fakeProfileService }
+      ]
     })
     .compileComponents();
   });
