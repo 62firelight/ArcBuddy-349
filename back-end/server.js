12023693b4b4bf668183599b54256d2695502bee
Luke Tang <tanlu824@student.otago.ac.nz>
2022-08-01 11:57:50 +1200

    Removed commented code in server.js

--- back-end/server.js
+++ back-end/server.js
@@ -79,11 +79,3 @@ const initializeServer = async () => {
 };
 
 initializeServer();
-
-// const date1 = new Date();
-
-// setTimeout(() => {
-//     const date2 = new Date();
-
-//     console.log(`${date2.toLocaleString()} > ${date1.toLocaleString()} ? ${date2 > date1 ? 'yes' : 'no'}`);
-// }, 1000);
