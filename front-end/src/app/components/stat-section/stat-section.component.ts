7e5f48ecc28a54c6e32f85e4fa10b8728bb9dee1
Luke Tang <tanlu824@student.otago.ac.nz>
2022-08-02 13:19:49 +1200

    Added comments to updateStats()

--- front-end/src/app/components/stat-section/stat-section.component.ts
+++ front-end/src/app/components/stat-section/stat-section.component.ts
@@ -97,12 +97,17 @@ export class StatSectionComponent implements OnInit {
 
     this.stats.clear();
 
-    var array1 = Array.from(this.section.keys());
-    var array2 = Object.keys(this.displayedStats);
+    // create array from the keys of our section map
+    let sectionKeys = Array.from(this.section.keys());
+    // create array from the keys of the displayedStats object
+    let displayedStatsKeys = Object.keys(this.displayedStats);
 
-    var includedStats = array1.filter(value => array2.includes(value));
-    var displayedStatsMap = new Map(Object.entries(this.displayedStats));
+    // create array from the union intersection of keys from both section and displayedStats
+    let includedStats = sectionKeys.filter(value => displayedStatsKeys.includes(value));
+    // create a map from the entries of displayedStats
+    let displayedStatsMap = new Map(Object.entries(this.displayedStats));
 
+    // create a new stats map with the stats that should be included in each section
     for (let name of includedStats) {
       const statName = this.section.get(name);
       if (statName == undefined) {
@@ -117,6 +122,7 @@ export class StatSectionComponent implements OnInit {
       this.stats.set(statName, statValue);
     }
 
+    // in case includedStats is empty, hide this stat section
     if (this.stats.size <= 0) {
       this.isVisible = false;
     } else {
