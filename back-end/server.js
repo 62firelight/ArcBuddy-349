630c509b5518e6bf42ee9ae97cbdd76b87f2c5a0
Luke Tang <tanlu824@student.otago.ac.nz>
2022-08-26 15:48:24 +1200

    Streamline retrieval of manifest

--- back-end/server.js
+++ back-end/server.js
@@ -22,6 +22,56 @@ app.use(cors());
 const apiUrl = `http://localhost:${port}`;
 const mongoUri = 'mongodb://127.0.0.1:27017';
 
+let isEmpty = exports.isEmpty = function isEmpty(path) {
+    return fs.readdirSync(path).length === 0;
+};
+
+let convertHash = exports.convertHash = function convertHash(hash) {
+    return hash >> 32;
+};
+
+let unzipManifest = exports.unzipManifest = async function unzipManifest() {
+    console.log('Unzipping manifest archive...');
+    try {
+        await extract('./manifest.zip', { dir: `${__dirname}/manifest` });
+        console.log('Extraction of manifest complete.');
+    } catch (err) {
+        console.log(err);
+        console.log('Something bad happened when unzipping the manifest.');
+    }
+};
+
+let loadManifest = exports.loadManifest = async function loadManifest() {
+    try {
+        const manifestFiles = fs.readdirSync('./manifest');
+
+        if (manifestFiles.length <= 0) {
+            throw ('No files in manifest folder!');
+        }
+
+        const manifestFile = manifestFiles[0];
+
+        db = new sqlite3.Database(`./manifest/${manifestFile}`);
+        console.log('Successfully loaded manifest.\n')
+
+        // db.serialize(() => {
+        //     const titanId = convertHash('2803282938');
+        //     // const adaId = convertHash('350061650');
+
+        //     // -1491684358
+        //     db.each(`SELECT * FROM DestinyRaceDefinition WHERE id = ${titanId};`, (err, row) => {
+        //         // console.log(row.id + ": " + row.info);
+        //         console.log(JSON.parse(row.json));
+        //         // console.log(JSON.stringify(row.json, null, 2));
+        //     });
+        // });
+        // db.close();
+    } catch (err) {
+        console.log(err);
+        console.log("Couldn't open Destiny Manifest.\n");
+    }
+};
+
 let destiny = undefined;
 exports.getDestiny = function getDestiny() {
     return destiny;
@@ -35,11 +85,12 @@ exports.getAccessToken = function getAccessToken() {
 let noDb = false;
 exports.getNoDb = function getNoDb() {
     return noDb;
-}
+};
 
-function isEmpty(path) {
-    return fs.readdirSync(path).length === 0;
-}
+let db = undefined;
+exports.getDb = function getDb() {
+    return db;
+};
 
 // read command line arguments if they were passed in
 if (process.argv.length > 2) {
@@ -71,7 +122,7 @@ const initializeServer = async () => {
         console.log(`Attempting to connect to MongoDB server at ${mongoUri}...`);
         await mongoose.connect(mongoUri, { serverSelectionTimeoutMS: 2000 });
         console.log('Successfully connected to MongoDB through Mongoose.\n');
-    } catch (error) {
+    } catch (err) {
         console.log(`Couldn't connect to MongoDB through Mongoose. Using in-memory database.\n`);
         noDb = true;
     }
@@ -92,67 +143,57 @@ const initializeServer = async () => {
         destiny = quria.destiny2;
 
         console.log('Successfully connected to Bungie.net API.\n');
-    } catch (error) {
-        console.error(error);
-        console.error('Failed to connect to Bungie.net API. Terminating server.\n');
+    } catch (err) {
+        console.log(err);
+        console.log('Failed to connect to Bungie.net API. Terminating server.\n');
         process.exit();
     }
     // 3, '4611686018468181342', '2305843009301648414' (Exo Hunter)
 
     // Download Destiny manifest
-    if (isEmpty('./manifest') == false || fs.existsSync('./manifest.zip')) {
-        console.log('Manifest file and/or archive detected.')
+    if (isEmpty('./manifest') == false && fs.existsSync('./manifest.zip')) {
+        console.log('Manifest file detected.\n');
+    } else if (isEmpty('./manifest') && fs.existsSync('./manifest.zip')) {
+        console.log('Manifest archive detected but no manifest file detected. Unzipping manifest...');
+        await unzipManifest();
+        await loadManifest();
     } else {
-        // destiny.GetDestinyManifest()
-        //     .then((response) => {
-        //         // console.log(JSON.stringify(response, null, 2));
-        //         if (response.Response.mobileWorldContentPaths.en === undefined) {
-        //             throw ('Manifest not found');
-        //         }
+        console.log('No manifest file and/or archive detected. Attempting to download manifest...');
+        await new Promise(async (resolve, reject) => {
+            try {
+                // fetch manifest from API
+                const manifestResponse = await destiny.GetDestinyManifest();
 
-        //         // manifest location
-        //         const manifestLocation = `https://www.bungie.net${response.Response.mobileWorldContentPaths.en}`;
-        //         console.log(`Manifest located at ${manifestLocation}`);
-        //         const file = fs.createWriteStream("manifest.zip");
+                // check if API response was correct
+                if (manifestResponse.Response.mobileWorldContentPaths.en === undefined) {
+                    throw ('Manifest not found.');
+                }
 
-        //         console.log('Manifest location found. Downloading manifest...');
-        //         https.get(manifestLocation, (manifest) => {
-        //             manifest.pipe(file);
+                const manifestLocation = `https://www.bungie.net${manifestResponse.Response.mobileWorldContentPaths.en}`;                
+                const file = fs.createWriteStream("manifest.zip");
+                
+                // download manifest
+                console.log('Manifest location found. Downloading manifest...');
+                https.get(manifestLocation, (manifest) => {
+                    manifest.pipe(file);
+                    file.on("finish", async () => {
+                        file.close();
+                        console.log("Download of manifest completed.");
 
-        //             file.on("finish", () => {
-        //                 file.close();
-        //                 console.log("Download of manifest completed");
-        //             });
-        //         });
+                        await unzipManifest();
+                        await loadManifest();
 
-        //     })
-        //     .catch((error) => {
-        //         console.log(error);
-        //     });
-    }
-
-    // Unzip Destiny manifest
-    if (isEmpty('./manifest') && fs.existsSync('./manifest.zip')) {
-        console.log('Unzipping manifest archive...');
-        try {
-            await extract('./manifest.zip', { dir: `${__dirname}/manifest` });
-            console.log('Extraction of manifest complete');
-        } catch (error) {
-            console.error(error);
-            console.error('Something bad happened when unzipping the manifest.');
-        }
-    }
-
-    const db = new sqlite3.Database('./manifest/world_sql_content_c0b6f372037834a3fc8e8f12c3b02363.content');
-
-    db.serialize(() => {
-        db.each("SELECT json FROM DestinyRaceDefinition WHERE id = -1491684358;", (err, row) => {
-            // console.log(row.id + ": " + row.info);
-            console.log(JSON.stringify(row.json, null, 2));
+                        resolve();
+                    }).on('error', err => {
+                        throw(err);
+                    });
+                });
+            } catch (err) {
+                console.log(err);
+                reject();
+            }
         });
-    });
-
-    db.close();
+    }
 
     // Attempt to fetch access token
     // console.log('Attempting to fetch access token...')
@@ -161,8 +202,8 @@ const initializeServer = async () => {
     // if (refreshResponse.access_token != undefined) {
     //     accessToken = refreshResponse.access_token;
     // } else {
-    //     console.error(error);
-    //     console.error('Failed to fetch access token. Terminating server.\n');
+    //     console.log(error);
+    //     console.log('Failed to fetch access token. Terminating server.\n');
     //     process.exit();
     // }
     // console.log(`Access token: ${accessToken}`);
