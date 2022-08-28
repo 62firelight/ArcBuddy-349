d27b8f5aa673d347dd443c3e91d0e5c6e7417cc7
Luke Tang <tanlu824@student.otago.ac.nz>
2022-08-28 18:57:55 +1200

    Fix failing tests

--- front-end/src/app/services/manifest.service.spec.ts
+++ front-end/src/app/services/manifest.service.spec.ts
@@ -1,13 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { ManifestService } from './manifest.service';
 
 describe('ManifestService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
   let service: ManifestService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(ManifestService);
+
+    // Inject the HTTP service and test controller for each test
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
