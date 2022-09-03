57643c948a3ec3b43994f46a59792adf72512aba
Luke Tang <tanlu824@student.otago.ac.nz>
2022-09-03 18:08:10 +1200

    Add transition for character arrows
    Closes #23.

--- front-end/src/app/components/characters/characters.component.ts
+++ front-end/src/app/components/characters/characters.component.ts
@@ -1,3 +1,4 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Character } from 'src/app/Character';
 import { ProfileService } from 'src/app/services/profile.service';
@@ -5,7 +6,32 @@ import { ProfileService } from 'src/app/services/profile.service';
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.component.html',
-  styleUrls: ['./characters.component.css']
+  styleUrls: ['./characters.component.css'],
+  animations: [
+    trigger(
+      'inOutAnimation', 
+      [
+        // fade in
+        transition(
+          ':enter', 
+          [
+            style({ height: 0, opacity: 0 }),
+            animate('0.25s ease-out', 
+                    style({ }))
+          ]
+        ),
+        // fade out
+        transition(
+          ':leave', 
+          [
+            style({ }),
+            animate('0.05s ease-in', 
+                    style({ height: 0, opacity: 0 }))
+          ]
+        )
+      ]
+    )
+  ]
 })
 export class CharactersComponent implements OnInit {
   @Input()
