c9bf24aa777f7967d1bbe4a00918676bf8e55495
Luke Tang <tanlu824@student.otago.ac.nz>
2022-09-03 18:25:34 +1200

    Add animation to landing page header

--- front-end/src/app/components/landing-page/landing-page.component.ts
+++ front-end/src/app/components/landing-page/landing-page.component.ts
@@ -1,9 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+  styleUrls: ['./landing-page.component.css'],
+  animations: [
+    trigger(
+      'inOutAnimation', 
+      [
+        // fade in
+        transition(
+          ':enter', 
+          [
+            style({ height: 0, opacity: 0 }),
+            animate('0.5s ease-out', 
+                    style({ }))
+          ]
+        ),
+        // fade out
+        transition(
+          ':leave', 
+          [
+            style({ }),
+            animate('0.25s ease-in', 
+                    style({ height: 0, opacity: 0 }))
+          ]
+        )
+      ]
+    )
+  ]
 })
 export class LandingPageComponent implements OnInit {
 
