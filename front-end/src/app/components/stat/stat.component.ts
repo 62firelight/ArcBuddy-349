1d5eb58dd0552343d9fcb4212da7b88c9334cb48
Luke Tang <tanlu824@student.otago.ac.nz>
2022-09-03 18:21:27 +1200

    Add animation to stat text

--- front-end/src/app/components/stat/stat.component.ts
+++ front-end/src/app/components/stat/stat.component.ts
@@ -1,10 +1,36 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { DestinyStatPipe } from '../pipes/destinyStat/destiny-stat.pipe';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-stat',
   templateUrl: './stat.component.html',
-  styleUrls: ['./stat.component.css']
+  styleUrls: ['./stat.component.css'],
+  animations: [
+    trigger(
+      'inOutAnimation', 
+      [
+        // fade in
+        transition(
+          ':enter', 
+          [
+            style({ opacity: 0 }),
+            animate('0.1s ease-out', 
+                    style({ }))
+          ]
+        ),
+        // fade out
+        transition(
+          ':leave', 
+          [
+            style({ }),
+            animate('0.1s ease-in', 
+                    style({ opacity: 0 }))
+          ]
+        )
+      ]
+    )
+  ]
 })
 export class StatComponent implements OnInit {
 
