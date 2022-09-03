0ca327f0adb948c9ce22c6d1d7d898628b13bf65
Luke Tang <tanlu824@student.otago.ac.nz>
2022-09-03 18:52:36 +1200

    Add animation to search bar

--- front-end/src/app/components/search-page/search-page.component.ts
+++ front-end/src/app/components/search-page/search-page.component.ts
@@ -1,13 +1,39 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDrawer, MatSidenav } from '@angular/material/sidenav';
 import { Subject } from 'rxjs';
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Profile } from 'src/app/Profile';
 import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
-  styleUrls: ['./search-page.component.css']
+  styleUrls: ['./search-page.component.css'],
+  animations: [
+    trigger(
+      'inOutAnimation', 
+      [
+        // fade in
+        transition(
+          ':enter', 
+          [
+            style({ height: 0, opacity: 0 }),
+            animate('0.25s ease-out', 
+                    style({ }))
+          ]
+        ),
+        // fade out
+        transition(
+          ':leave', 
+          [
+            style({ }),
+            animate('0.25s ease-in', 
+                    style({ height: 0, opacity: 0 }))
+          ]
+        )
+      ]
+    )
+  ]
 })
 export class SearchPageComponent implements OnInit {
 
