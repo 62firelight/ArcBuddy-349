f1f28bb3cbd3bd57caf9aa780c851b9ca90fa7eb
Luke Tang <tanlu824@student.otago.ac.nz>
2022-09-05 16:02:01 +1200

    Add partial test for submitting invalid input
    References #25.

--- front-end/src/app/components/search/search.component.spec.ts
+++ front-end/src/app/components/search/search.component.spec.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { DebugElement } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
 import { SearchComponent } from './search.component';
@@ -30,12 +30,7 @@ describe('SearchComponent', () => {
   //   expect(component).toBeTruthy();
   // });
 
-  // TODO: Refactor search component to be either completely template-driven or reactive, not both
   it('submits no input', () => {
-    // const searchButton = debugElement.query(
-    //   By.css('[data-testid="search-button"]')
-    // );
-    // searchButton.triggerEventHandler('click', null);
     // get form element
     const searchForm = debugElement.query(
       By.css('[data-testid="search-form"]')
@@ -48,4 +43,39 @@ describe('SearchComponent', () => {
     expect(component.error)
       .toBe('Error occurred when parsing Bungie Name. A Bungie Name should formatted similarly to "name#1234".');
   });
+
+  // it('submits invalid input', fakeAsync(async () => {
+  //   // Act
+  //   const result = 1 + 1;
+
+  //   const searchInput = debugElement.query(
+  //     By.css('[data-testid="search-input"]')
+  //   );
+  //   const searchInputEl = searchInput.nativeElement;
+  //   // set invalid field value
+  //   searchInputEl.value = 'Guardian#1234';
+  //   // dispatch input event
+  //   const event = document.createEvent('Event');
+  //   event.initEvent('input', true, false);
+  //   fixture.detectChanges();
+  //   // searchInput.triggerEventHandler('input');
+  //   // dispatchEvent(new Event('input'));
+  //   searchInputEl.dispatchEvent(new Event('input'));
+
+  //   tick(1000);
+
+  //   fixture.detectChanges();
+  //   // get form element
+  //   const searchForm = debugElement.query(
+  //     By.css('[data-testid="search-form"]')
+  //   );
+  //   // submit form
+  //   searchForm.triggerEventHandler('ngSubmit', null);
+  //   fixture.detectChanges();
+    
+  //   console.log(searchInputEl.value, component.name, component.error);
+
+  //   // Assert
+  //   expect(result).toBe(2);
+  // }));
 });
