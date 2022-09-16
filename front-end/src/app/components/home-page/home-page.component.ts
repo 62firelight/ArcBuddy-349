93c9888b4593082a064bacbbd70ae2165522efae
Luke Tang <tanlu824@student.otago.ac.nz>
2022-09-16 14:06:38 +1200

    Add logo animation on hover

--- front-end/src/app/components/home-page/home-page.component.ts
+++ front-end/src/app/components/home-page/home-page.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { Subject } from 'rxjs';
@@ -7,7 +8,15 @@ import { ProfileService } from 'src/app/services/profile.service';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
+  animations: [
+    trigger('move', [
+      state('in', style({ transform: '*' })),
+      state('out', style({ transform: 'rotate(360deg)' })),
+      transition('in => out', animate('1s')),
+      transition('out => in', animate('0s'))
+    ]),
+  ]
 })
 export class HomePageComponent implements OnInit {
 
@@ -15,9 +24,12 @@ export class HomePageComponent implements OnInit {
 
   changingStats = new Subject<Profile>();
 
-  addingProfiles  = new Subject<Profile>();
+  addingProfiles = new Subject<Profile>();
 
   changingProfiles = new Subject<Profile>();
+
+  state = 'in';
+  mouseover = false;
 
   @ViewChild('sidenav')
   sidenav!: MatSidenav;
@@ -25,6 +37,30 @@ export class HomePageComponent implements OnInit {
   constructor(private profileService: ProfileService) { }
 
   ngOnInit(): void {
+  }
+
+  // source: https://stackoverflow.com/questions/44535108/how-do-i-perform-infinite-animations-in-angular-2 
+  startAnimation() {
+    this.mouseover = true;
+    this.state = 'out';
+  }
+
+  stopAnimation() {
+    this.mouseover = false;
+    this.state = 'in';
+  }
+
+  onEnd(event: { toState: string; }) {
+    if (!this.mouseover) {
+      return;
+    }
+
+    this.state = 'in';
+    if (event.toState === 'in') {
+      setTimeout(() => {
+        this.state = 'out';
+      }, 0);
+    }
   }
 
 }
